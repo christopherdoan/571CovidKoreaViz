72501e9cb98cc8314a090bc6404566e236484f45
coreykoz <ckozlovski2017@gmail.com>
2021-04-29 16:08:19 -0400

    implemented filtering

--- korea-viz/src/Charts/timeseries.js
+++ korea-viz/src/Charts/timeseries.js
@@ -5,8 +5,11 @@ let initTimeSeries = (data) => {
   let width = 1000;
 	let height = 600;
   let padding = 40;
-  let bins = [true, true, true, true, true, true, true, true, true];
-
+  let bins = [
+    [true, false], //confirmed or deceased
+    [true, false, false], //sex: all, male(used in html), female(used in html), male(Actual), female(actual)
+    [true, true, true, true, true, true, true, true, true, true]//order of checked: All, 0s, 10s... 80s
+  ];
   let svg = d3.select('#time').attr('width', width).attr('height', height);
   svg.append('rect')
     .attr('class', 'background')
@@ -33,33 +36,75 @@ let initTimeSeries = (data) => {
       .attr("transform", "translate(" + (padding) + ",0)")
       .call(yAxis);
 
-  function update(data){
+  let updateBins = (newBins) => {
+    bins = newBins;
+    console.log(bins[1]);
 
+    // set dataset
+    let curr_data = Object.values(data.confirmedData)
+    if (bins[0][1] == true){
+      curr_data = Object.values(data.deceasedData);
+    }
+   
     // create x axis
-    xScale.domain([d3.min(data, function(d){
-        return d.date;
-      }),
-      d3.max(data, function(d){
-        return d.date;
-      })
-      ]);
+    xScale.domain([d3.min(curr_data, function(d){
+      return d.date;
+    }),
+    d3.max(curr_data, function(d){
+      return d.date;
+    })
+    ]);
     svg.selectAll(".axisX").transition()
       .duration(300)
       .call(xAxis);
 
+    // create ratio variable for future calcuations in relation to age groups
+    var sexRatio = 1;
+
     // create y axis
-    yScale.domain([d3.max(data, function(d){
-      return d.total;
-    }), d3.min(data, function(d){
-      return d.total;
+    yScale.domain([d3.max(curr_data, function(d){
+          // check sex distributions and modify working data
+        if (bins[1][1] && !bins[1][2]){
+          d.curr_value = d.male;
+        } else if (!bins[1][1] && bins[1][2]){
+          d.curr_value = d.female;
+        } else {
+          d.curr_value = d.total;
+        }
+
+        // create/update sexRatio value used to caluculate per age group
+        if (d.total != 0){
+            sexRatio = d.curr_value/d.total;
+        }
+        // arr of counts for each age group, used in tandem with ageArr to select the correct age groups
+        var ageArr = bins[2].slice(1);
+        var ageArrValues = [d.age_0, d.age_10, d.age_20, d.age_30, d.age_40, d.age_50, d.age_60, d.age_70, d.age_80];
+        
+        // calcuate subtotal based on age groups
+        var subtotal = 0;
+        if (ageArr.every( e  => e == true)){
+            return d.curr_value;
+        } else {
+            for (var i = 0; i < 9; i++){
+                if (ageArr[i]){
+                    subtotal = subtotal + ageArrValues[i];
+                }
+            }
+        }
+        // apply ratio to subtotal
+        d.curr_value = Math.round(subtotal * sexRatio);
+        return d.curr_value; 
+
+    }), d3.min(curr_data, function(d){
+      return d.curr_value;
     })]);
     svg.selectAll(".axisY")
       .transition()
       .duration(300)
       .call(yAxis);
 
-    var line = svg.selectAll(".line")
-      .data([data], function(d){ return d.ser1 });
+      var line = svg.selectAll(".line")
+      .data([curr_data], function(d){ return d.ser1 });
 
     line.enter()
       .append("path")
@@ -69,26 +114,16 @@ let initTimeSeries = (data) => {
       .duration(300)
       .attr("d", d3.line()
         .x(function(d){
-          console.log(typeof(d.date));
           return xScale(d.date);
         })
         .y(function(d) {
-          return yScale(d.total);
+          return yScale(d.curr_value);
         }))
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 2.5);
   }
-
-  let dataArr = Object.values(data);
-
-  // draw whole graph
-  update(Object.values(data.deceasedData));
-
-  let updateBins = (newBins) => {
-    bins = newBins;
-    console.log(bins);
-  }
+  updateBins(bins);
 
   // let toggle = () => {
   //
--- korea-viz/src/VisOverlay.jsx
+++ korea-viz/src/VisOverlay.jsx
@@ -16,7 +16,12 @@ class VisOverlay extends Component {
   constructor(props) {
     super(props);
     autoBind(this);
-    this.state = {loading: true, mode: props.mode, checked: [true, true, true, true, true, true, true, true, true, true]}; //order of checked: All, 0s, 10s... 80s
+    this.state = {loading: true, mode: props.mode, 
+      checked: [
+        [true, false],
+        [true, false, false],
+        [true, true, true, true, true, true, true, true, true, true]//order of checked: All, 0s, 10s... 80s
+      ]}; 
   }
   componentDidMount(){
     // this.toggleMap = init(data);
@@ -51,9 +56,10 @@ class VisOverlay extends Component {
     console.log(e.target.value);
     console.log(e.target.valueAsNumber);
   }
-  selectAll(e) {
-    let tempChecked = [!this.state.checked[0]];
-    if(this.state.checked[0]){
+  selectAllAge(e) {
+    let tempChecked = [!this.state.checked[2][0]];
+    let checker = arr => arr.every(Boolean);
+    if(checker(this.state.checked[2])){
       for(let i = 0; i < 9; i++){
         tempChecked.push(false);
       }
@@ -62,14 +68,36 @@ class VisOverlay extends Component {
         tempChecked.push(true);
       }
     }
-    this.updateBins(tempChecked.slice(1, tempChecked.length));
-    this.setState({checked: tempChecked});
+    this.updateBins([this.state.checked[0], this.state.checked[1], tempChecked]);
+    this.setState({checked: [this.state.checked[0], this.state.checked[1],tempChecked]});
+  }
+  selectData(e){
+    let tempArr = this.state.checked[0]
+    if (tempArr[0]){
+      tempArr = [false, true];
+    } else {
+      tempArr = [true, false];
+    }
+    this.updateBins([tempArr, this.state.checked[1], this.state.checked[2]]);
+    this.setState({checked: [tempArr, this.state.checked[1],this.state.checked[2]]});
+  }
+  onSexChange(e){
+    console.log(e.target.value);
+    let tempArr = [false, false, false];
+    if (e.target.value === "Male"){
+      tempArr = [false, true, false];
+    } else if (e.target.value === "Female"){
+      tempArr = [false, false, true];
+    } else {
+      tempArr = [true, false, false];
+    }
+    this.updateBins([this.state.checked[0], tempArr, this.state.checked[2]]);
+    this.setState({checked: [this.state.checked[0], tempArr, this.state.checked[2]]});
   }
   render() {
     // if(this.state.mode){
     //   this.draw(this.state.mode);
     // }
-    console.log(this.state.mode);
     return <div className={`vis-overlay ${this.state.mode || this.state.loading ? "active" : ""}`} onClick={this.handleClick} ref={overlay => this.overlay = overlay}>
         <div className={`loading-overlay ${this.state.loading ? "active" : ""}`}>
           Loading...
@@ -85,36 +113,39 @@ class VisOverlay extends Component {
 
           <form action="" >
             <p>Covid Trends by:</p>
-            <input type="radio" id="confirmed" name="val" value="Confirmed" checked />
+            <input type="radio" id="confirmed" name="val" value="Confirmed" checked={this.state.checked[0][0]} onChange={this.selectData} />
             <label for="confirmed">Confirmed</label><br/>
-            <input type="radio" id="deceased" name="val" value="Deceased" />
+            <input type="radio" id="deceased" name="val" value="Deceased" checked={this.state.checked[0][1]} onChange={this.selectData} />
             <label for="deceased">Deceased</label><br/>
 
             <br/>
             <p>Sex:</p>
-            <input type="radio" id="all" name="sex" value="all" />
-            <label for="all">All</label><br/>
-            <input type="radio" id="male" name="sex" value="Male" />
-            <label for="male">Male</label><br/>
-            <input type="radio" id="female" name="sex" value="Female" />
-            <label for="female">Female</label><br/>
+           
+              <input type="radio" id="all" name="sex" value="all" checked={this.state.checked[1][0]} onChange={this.onSexChange}/>
+              <label for="all">All</label><br/>
+              <input type="radio" id="male" name="sex" value="Male" checked={this.state.checked[1][1]} onChange={this.onSexChange} />
+              <label for="male">Male</label><br/>
+              <input type="radio" id="female" name="sex" value="Female" checked={this.state.checked[1][2]} onChange={this.onSexChange}/>
+              <label for="female">Female</label><br/>
+            
 
             <br/>
             <p>Age Group:</p>
-            <input id="typeinp" type="range" min="0" max="5" defaultValue="3" step="0" onChange={this.onChange1}/>
-            <input type="checkbox" id="selectAll" name="selectAll" value="selectAll" checked={this.state.checked[0]} onChange={this.selectAll} />
+            <input type="checkbox" id="selectAll" name="selectAll" value="selectAll" checked={this.state.checked[2][0]} onChange={this.selectAllAge} />
             <label for="selectAll">All</label><br/>
             {
-              this.state.checked.slice(1, this.state.checked.length).map((e, i) => {
+              this.state.checked[2].map((e, i) => {
                 return (
                       <div className="age-checkbox">
-                        <input type="checkbox" id={`${i}s`} name={`checkbox${i}`} value={`${i}s`} checked={e} onChange={()=>{
-                          let tempChecked = this.state.checked;
+                        <input type="checkbox" id={`${i}s`} name={`checkbox${i}`} value={`${i}s`} checked={this.state.checked[2][i+1]} onChange={()=>{
+
+                          let tempChecked = this.state.checked[2];
                           tempChecked[i+1] = !tempChecked[i+1];
-                          this.updateBins(tempChecked.slice(1, tempChecked.length));
-                          this.setState({checked: tempChecked});
+                          console.log(e);
+                          this.updateBins([this.state.checked[0], this.state.checked[1], tempChecked]);
+                          this.setState({checked: [this.state.checked[0], this.state.checked[1],tempChecked]});
                         }} />
-                        <label for={`${i}s`}>{`${i}s`}</label><br/>
+                        <label for={`${i}s`}>{`${i}0s`}</label><br/>
                       </div>
                     );
               })
